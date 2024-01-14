aa0230333451239d474a768b13d66e60e92d8cc1
garrett <garrettchollett@yahoo.com>
2024-01-14 17:22:52 -0600

    first commit

---
+++ src/components/TheAbout.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import AboutItem from './AboutItem.vue';
+import { useCounterStore } from '@/stores/counter';
+
+const counter = useCounterStore();
+// counter.increment()
+defineProps<{
+    desc: string
+}>()
+</script>
+<template>
+    <div>
+        <AboutItem>
+            <template #icon> <button @click="counter.increment">{{ counter.count }}</button> </template>
+            <template #heading> This is the about page </template>
+            <p>
+                {{ desc }}
+            </p>
+        </AboutItem>
+        <AboutItem>
+            <template #icon> World </template>
+            <template #heading> I am learning </template>
+            <p>
+                Yay!
+            </p>
+        </AboutItem>
+    </div>
+</template>
+<style scoped lang="scss">
+</style>
